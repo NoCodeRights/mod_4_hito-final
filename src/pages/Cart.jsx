a759740c85df4360289c91b48aba818b2e373874
Marcelo Sepulveda <marcelo.sepulveda.ramirez@gmail.com>
2024-10-03 21:16:15 -0300

    all requeriments met

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -2,9 +2,61 @@ import React, { useContext } from "react";
 import pizzas from "../pizzas";
 import { formatNumber } from "../scripts";
 import { CartContext } from "../context/CartContext";
+import { Link } from "react-router-dom";
+import { AuthContext } from "../context/AuthContext";
+import Swal from "sweetalert2";
 
 const Cart = () => {
-  const { cartItems, addToCart, decreaseQuantity, calculateTotal } = useContext(CartContext);
+  const { cartItems, addToCart, decreaseQuantity, calculateTotal } =
+    useContext(CartContext);
+  const { token } = useContext(AuthContext);
+
+  const checkoutCart = async () => {
+    try {
+      const response = await fetch("http://localhost:5000/api/checkouts", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+        body: JSON.stringify({ Cart, totalAmount: calculateTotal }),
+      });
+
+      if (!response.ok) {
+        throw new Error("Error al procesar el pago. Intenta nuevamente.");
+      }
+
+      const result = await response.json();
+
+      Swal.fire({
+        title: "Pago exitoso",
+        text: `Tu compra se ha realizado con éxito`,
+        icon: "success",
+        confirmButtonText: "Cerrar",
+      });
+    } catch (error) {
+      Swal.fire({
+        title: "Error",
+        text: error.message,
+        icon: "error",
+        confirmButtonText: "Cerrar",
+      });
+    }
+  };
+
+  const validateAuth = () => {
+    if (!token) {
+      Swal.fire({
+        title: "Error!",
+        text: "Debes iniciar sesión para poder realizar el pago",
+        icon: "error",
+        confirmButtonText: "Cerrar",
+      });
+      return;
+    }
+
+    checkoutCart();
+  };
 
   return (
     <div>
@@ -23,7 +75,9 @@ const Cart = () => {
                   </span>
                   <span>{pizza.pizzaPrice * pizza.quantity}</span>
                   <div>
-                    <button onClick={() => decreaseQuantity(pizza.id)}>-</button>
+                    <button onClick={() => decreaseQuantity(pizza.id)}>
+                      -
+                    </button>
                     <button onClick={() => addToCart(pizza)}>+</button>
                   </div>
                 </div>
@@ -31,7 +85,7 @@ const Cart = () => {
             ))}
           </ul>
           <h3>Total: ${formatNumber(calculateTotal())}</h3>
-          <button>Pagar</button>
+          <button onClick={() => validateAuth()}>Pagar</button>
         </div>
       )}
     </div>
